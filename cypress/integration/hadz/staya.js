b67a816131a5001687c21f8015617af6ccbb8346
Julia Morozovskaya <116824115+Julia-Morozovskaya@users.noreply.github.com>
2022-12-14 00:14:23 +0500

    Update staya.js

--- cypress/integration/hadz/staya.js
+++ cypress/integration/hadz/staya.js
@@ -3,8 +3,8 @@ describe('Тестирование staya', function () {
     it('Форма логина и пароля', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('ju.celikova@gmail.com');
-        cy.get('.auth-form > form > [type="password"]').type('YUlia123');
+        cy.get('.auth-form > form > [type="email"]').type('log');
+        cy.get('.auth-form > form > [type="password"]').type('pass');
         cy.get('.auth-form__submit > .s-button__content').click();
         cy.contains('Мои заказы');
         cy.get('button.profile__nav-link').click();
@@ -13,8 +13,8 @@ describe('Тестирование staya', function () {
     it('Неправильный логин', function () {
        cy.visit('https://staya.dog/');
        cy.get('.header-bottom__right--link').click();
-       cy.get('.auth-form > form > [type="email"]').type('JU.celikova@gma.com');
-       cy.get('.auth-form > form > [type="password"]').type('YUlia123');
+       cy.get('.auth-form > form > [type="email"]').type('log');
+       cy.get('.auth-form > form > [type="password"]').type('pass');
        cy.get('.auth-form__submit > .s-button__content').click('');
        cy.contains('Невозможно войти с предоставленными учетными данными');
     })     
